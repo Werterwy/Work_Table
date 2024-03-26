416d4e96ee2b13260ab6b4006f22ede4a74e57ad
Orazbay <you@biy.kz>
2024-03-26 12:07:34 +0500

    создание таблицы рамерами 6x4

--- app/src/main/java/com/example/worktable/MainActivity.java
+++ app/src/main/java/com/example/worktable/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private GestureDetector gestureDetector;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
